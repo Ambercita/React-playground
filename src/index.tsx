9f05a9b75598e4c28caf96db5243c75441a7874b
Jia Wang <Ambercitaaaa@gmail.com>
2024-04-07 18:54:04 +0200

    TypeScript y Sass incorporados

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>hhahah</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
